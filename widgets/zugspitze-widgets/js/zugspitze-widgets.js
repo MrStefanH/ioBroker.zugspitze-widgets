2a73202b438c885908849bc8157ccb8745ef97ef
Stefan Pfaffe <pfaffe.st@gmail.com>
2023-02-19 13:22:05 +0100

    Delete unused stuff

--- widgets/zugspitze-widgets/js/zugspitze-widgets.js
+++ widgets/zugspitze-widgets/js/zugspitze-widgets.js
@@ -38,37 +38,6 @@ vis.binds["zugspitze-widgets"] = {
             console.log('Version zugspitze-widgets: ' + vis.binds["zugspitze-widgets"].version);
             vis.binds["zugspitze-widgets"].version = null;
         }
-    },
-    createWidget: function (widgetID, view, data, style) {
-        var $div = $('#' + widgetID);
-        // if nothing found => wait
-        if (!$div.length) {
-            return setTimeout(function () {
-                vis.binds["zugspitze-widgets"].createWidget(widgetID, view, data, style);
-            }, 100);
-        }
-
-        var text = '';
-        text += 'OID: ' + data.oid + '</div><br>';
-        text += 'OID value: <span class="zugspitze-widgets-value">' + vis.states[data.oid + '.val'] + '</span><br>';
-        text += 'Color: <span style="color: ' + data.myColor + '">' + data.myColor + '</span><br>';
-        text += 'extraAttr: ' + data.extraAttr + '<br>';
-        text += 'Browser instance: ' + vis.instance + '<br>';
-        text += 'htmlText: <textarea readonly style="width:100%">' + (data.htmlText || '') + '</textarea><br>';
-
-        $('#' + widgetID).html(text);
-
-        // subscribe on updates of value
-        function onChange(e, newVal, oldVal) {
-            $div.find('.template-value').html(newVal);
-        }
-        if (data.oid) {
-            vis.states.bind(data.oid + '.val', onChange);
-            //remember bound state that vis can release if didnt needed
-            $div.data('bound', [data.oid + '.val']);
-            //remember onchange handler to release bound states
-            $div.data('bindHandler', onChange);
-        }
     }
 };
 
