66341069bd0f8b9a3b18a7cd6db07a20c78e50c3
Stefan Pfaffe <pfaffe.st@gmail.com>
2023-02-19 21:11:08 +0100

    Update version number

--- widgets/zugspitze-widgets/js/zugspitze-widgets.js
+++ widgets/zugspitze-widgets/js/zugspitze-widgets.js
@@ -1,7 +1,7 @@
 /*
     ioBroker.vis zugspitze-widgets Widget-Set
 
-    version: "0.0.17"
+    version: "0.0.18"
 
     Copyright 2023 MonkeyParson pfaffe.st@gmail.com
 */
@@ -32,7 +32,7 @@ $.extend(
 
 // this code can be placed directly in zugspitze-widgets.html
 vis.binds["zugspitze-widgets"] = {
-    version: "0.0.17",
+    version: "0.0.18",
     showVersion: function () {
         if (vis.binds["zugspitze-widgets"].version) {
             console.log('Version zugspitze-widgets: ' + vis.binds["zugspitze-widgets"].version);
