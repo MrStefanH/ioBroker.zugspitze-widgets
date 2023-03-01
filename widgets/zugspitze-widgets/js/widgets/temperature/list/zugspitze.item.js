40456d741027eeed35a84e9b4bb019906835b7e8
Stefan Pfaffe <pfaffe.st@gmail.com>
2023-03-01 20:52:06 +0100

    Add new list item for temperature

--- widgets/zugspitze-widgets/js/widgets/temperature/list/zugspitze.item.js
+++ widgets/zugspitze-widgets/js/widgets/temperature/list/zugspitze.item.js
@@ -7,39 +7,35 @@
 */
 "use strict";
 
-vis.binds["zugspitze-widgets"].alertlistitem = {
+vis.binds["zugspitze-widgets"].temperaturelistitem = {
     getDataFromJson(obj) {
         return {
             oid: obj.oid,
             debug: obj.debug,
             label: obj.label,
-            switchState: obj.switchState,
-            textOnTrue: obj.textOnTrue,
-            textOnFalse: obj.textOnFalse
+            refTemperature: obj.refTemperature
         }
     },
     createWidget: function (el, data) {
-        let widgetName = 'Alert List Item';
-        let logPrefix = `[Alert List Item - ${data.wid}] initialize:`;
+        let widgetName = 'Temperature List Item';
+        let logPrefix = `[Temperature List Item - ${data.wid}] initialize:`;
 
         try {
             let $this = $(el);
 
             if (!$this.length) {
                 return setTimeout(function () {
-                    vis.binds["zugspitze-widgets"].alertlistitem.createWidget(el, data);
+                    vis.binds["zugspitze-widgets"].temperaturelistitem.createWidget(el, data);
                 }, 100);
             }
 
             $this.html(`
                 <li class="list-group-item pt-0">
                     <p class="specs">${data.label}</p>
-                    <div class="ml-auto zugspitze-alert-state-html-element"
+                    <div class="ml-auto zugspitze-temperature-state-html-element"
                         zugspitze-oid='${data.oid}'
                         zugspitze-debug='${data.debug}'
-                        zugspitze-switchState='${data.switchState}'
-                        zugspitze-textOnTrue='${data.textOnTrue}'
-                        zugspitze-textOnFalse='${data.textOnFalse}'
+                        zugspitze-refTemperature='${data.refTemperature}'
                     ></div>
                 </li>
             `);
@@ -49,19 +45,17 @@ vis.binds["zugspitze-widgets"].alertlistitem = {
 	}
 }
 
-$.initialize(".zugspitze-alert-list-item-html-element", function () {
+$.initialize(".zugspitze-temperature-list-item-html-element", function () {
     let $this = $(this);
-    let logPrefix = `[Alert List Item HTML Element]`;
+    let logPrefix = `[Temperature List Item HTML Element]`;
 
     try {
         zugspitzeHelper.extractHtmlWidgetData(
             $this,
-            vis.binds["zugspitze-widgets"].alertlistitem.getDataFromJson({
+            vis.binds["zugspitze-widgets"].temperaturelistitem.getDataFromJson({
                 debug: false,
                 label: '',
-                switchState: false,
-                textOnTrue: 'OK',
-                textOnFalse: 'FEHLER'
+                refTemperature: obj.refTemperature
             }),
             logPrefix,
             initializeHtml
@@ -69,7 +63,7 @@ $.initialize(".zugspitze-alert-list-item-html-element", function () {
 
         function initializeHtml(widgetData) {
             if (widgetData.debug) console.log(`${logPrefix} initialize widget`);
-            vis.binds["zugspitze-widgets"].alertlistitem.createWidget($this, widgetData);
+            vis.binds["zugspitze-widgets"].temperaturelistitem.createWidget($this, widgetData);
         }
     } catch (ex) {
         console.error(`${logPrefix} $.initialize: error: ${ex.message}, stack: ${ex.stack} `);
