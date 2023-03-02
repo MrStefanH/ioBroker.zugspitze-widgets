4a1997bfe6d1e21b005517deefdf3fb2d65b98f8
Stefan Pfaffe <pfaffe.st@gmail.com>
2023-03-02 21:15:00 +0100

    Add new list item widget

---
+++ widgets/zugspitze-widgets/js/widgets/type/shelly/list/zugspitze.item.js
@@ -0,0 +1,71 @@
+/*
+    ioBroker.vis zugspitze-widgets Widget-Set
+
+    version: "0.0.33"
+
+    Copyright 2023 MonkeyParson pfaffe.st@gmail.com
+*/
+"use strict";
+
+vis.binds["zugspitze-widgets"].typeshellylistitem = {
+    getDataFromJson(obj) {
+        return {
+            oid: obj.oid,
+            debug: obj.debug,
+            label: obj.label
+        }
+    },
+    createWidget: function (el, data) {
+        let widgetName = 'Type Shelly List Item';
+        let logPrefix = `[Type Shelly List Item - ${data.wid}] initialize:`;
+
+        try {
+            let $this = $(el);
+
+            if (!$this.length) {
+                return setTimeout(function () {
+                    vis.binds["zugspitze-widgets"].typeshellylistitem.createWidget(el, data);
+                }, 100);
+            }
+
+            $this.html(`
+                <li class="list-group-item pt-0">
+                    <p class="specs">${data.label}</p>
+                    <div class="ml-auto zugspitze-type-shelly-state-html-element"
+                        zugspitze-oid='${data.oid}'
+                        zugspitze-debug='${data.debug}'
+                    ></div>
+                </li>
+            `);
+        } catch (ex) {
+            console.error(`[${widgetName} - ${data.wid}] initialize: error: ${ex.message}, stack: ${ex.stack}`);
+        }
+	}
+}
+
+$.initialize(".zugspitze-type-shelly-list-item-html-element", function () {
+    let $this = $(this);
+    let logPrefix = `[Type Shelly List Item HTML Element]`;
+
+    try {
+        zugspitzeHelper.extractHtmlWidgetData(
+            $this,
+            vis.binds["zugspitze-widgets"].typeshellylistitem.getDataFromJson({
+                debug: false,
+                label: ''
+            }),
+            logPrefix,
+            initializeHtml
+        );
+
+        function initializeHtml(widgetData) {
+            if (widgetData.debug) console.log(`${logPrefix} initialize widget`);
+            vis.binds["zugspitze-widgets"].typeshellylistitem.createWidget($this, widgetData);
+        }
+    } catch (ex) {
+        console.error(`${logPrefix} $.initialize: error: ${ex.message}, stack: ${ex.stack} `);
+        $this.append(
+            `<div style = "background: FireBrick; color: white;">Error ${ex.message}</div >`
+        );
+    }
+});
