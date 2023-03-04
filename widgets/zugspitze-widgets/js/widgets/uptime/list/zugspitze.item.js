d29422e425b970361c54cc3aed74ea1b3240932c
Stefan Pfaffe <pfaffe.st@gmail.com>
2023-03-04 21:33:23 +0100

    Add list item for item

---
+++ widgets/zugspitze-widgets/js/widgets/uptime/list/zugspitze.item.js
@@ -0,0 +1,72 @@
+/*
+    ioBroker.vis zugspitze-widgets Widget-Set
+
+    version: "0.0.38"
+
+    Copyright 2023 MonkeyParson pfaffe.st@gmail.com
+*/
+"use strict";
+
+vis.binds["zugspitze-widgets"].uptimelistitem = {
+    getDataFromJson(obj) {
+        return {
+            oid: obj.oid,
+            debug: obj.debug,
+            durationFormat: obj.durationFormat
+        }
+    },
+    createWidget: function (el, data) {
+        let widgetName = 'Uptime List Item';
+        let logPrefix = `[Uptime List Item - ${data.wid}] initialize:`;
+
+        try {
+            let $this = $(el);
+
+            if (!$this.length) {
+                return setTimeout(function () {
+                    vis.binds["zugspitze-widgets"].uptimelistitem.createWidget(el, data);
+                }, 100);
+            }
+
+            $this.html(`
+                <li class="list-group-item pt-0">
+                    <p class="specs">${data.label}</p>
+                    <div class="ml-auto zugspitze-uptime-state-html-element"
+                        zugspitze-oid='${data.oid}'
+                        zugspitze-debug='${data.debug}'
+                        zugspitze-durationFormat='${data.durationFormat}'
+                    ></div>
+                </li>
+            `);
+        } catch (ex) {
+            console.error(`[${widgetName} - ${data.wid}] initialize: error: ${ex.message}, stack: ${ex.stack}`);
+        }
+	}
+}
+
+$.initialize(".zugspitze-uptime-list-item-html-element", function () {
+    let $this = $(this);
+    let logPrefix = `[Uptime List Item HTML Element]`;
+
+    try {
+        zugspitzeHelper.extractHtmlWidgetData(
+            $this,
+            vis.binds["zugspitze-widgets"].uptimelistitem.getDataFromJson({
+                debug: false,
+                durationFormat: 'd [Tage], h [Stunden], m [Minuten]'
+            }),
+            logPrefix,
+            initializeHtml
+        );
+
+        function initializeHtml(widgetData) {
+            if (widgetData.debug) console.log(`${logPrefix} initialize widget`);
+            vis.binds["zugspitze-widgets"].uptimelistitem.createWidget($this, widgetData);
+        }
+    } catch (ex) {
+        console.error(`${logPrefix} $.initialize: error: ${ex.message}, stack: ${ex.stack} `);
+        $this.append(
+            `<div style = "background: FireBrick; color: white;">Error ${ex.message}</div >`
+        );
+    }
+});
