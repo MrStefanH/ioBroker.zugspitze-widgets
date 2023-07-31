4fe90287ec21d24729464dcd83ba916cbf18baf4
Stefan Pfaffe <pfaffe.st@gmail.com>
2023-07-31 11:16:17 +0200

    Do another bug fix

--- widgets/zugspitze-widgets/js/widgets/details/shelly/zugspitze.state.js
+++ widgets/zugspitze-widgets/js/widgets/details/shelly/zugspitze.state.js
@@ -31,7 +31,7 @@ vis.binds["zugspitze-widgets"].detailsshellystate = {
             let overTemperature = data.deviceAlias + '.overtemperature';
             vis.conn.getStates(id, async (error, states) => {
                 let stateValue = states[id].val;
-                let overTemperatureValue = await zugspitzeHelper.getStateAsync(id);
+                let overTemperatureValue = await zugspitzeHelper.getStateAsync(overTemperature);
                 console.error('ASYNC VALUE ' + overTemperatureValue);
                 $this.html(`
                     <ul class="list-group borderless collapse" id="flush-collapse-${stateValue}">
